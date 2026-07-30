e58b9b1ad0aeb6f6c5184640de196c7ba020b3eb
Abdullah Tech <abdullah.omar.tech.7@gmail.com>
2026-07-30 20:37:25 +0300

    Test changes

--- src/MCFServiceProvider.php
+++ src/MCFServiceProvider.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 namespace MCF;
-
+use MCF\Support\MCFViewFinder;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\ServiceProvider;
 use MCF\Commands\MakeMigrationCommand;
@@ -36,13 +36,25 @@ class MCFServiceProvider extends ServiceProvider
             'mcf'
         );
 
-        $this->app->singleton(MakeMigrationCommand::class, function ($app) {
-            return new MakeMigrationCommand(
-                $app['migration.creator'],
-                $app['composer'],
-            );
-        });
 
+$this->app->singleton('view.finder', function ($app) {
+    return new MCFViewFinder(
+        $app['files'],
+        $app['config']['view.paths']
+    );
+});
+
+$this->app->resolving('view', function ($factory, $app) {
+
+    $reflection = new \ReflectionObject($factory);
+
+    if ($reflection->hasProperty('finder')) {
+        $property = $reflection->getProperty('finder');
+        $property->setAccessible(true);
+        $property->setValue($factory, $app['view.finder']);
+    }
+
+});
 
 
         $this->app->extend('translation.loader', function ($loader, $app) {
@@ -66,19 +78,7 @@ if ($filesystem->exists($modulesPath)) {
     foreach ($filesystem->directories($modulesPath) as $modulePath) {
         $module = basename($modulePath);
 
-        $viewPaths = [];
-
-        foreach ($filesystem->directories($modulePath) as $workflowPath) {
-            $viewsPath = $workflowPath . DIRECTORY_SEPARATOR . 'Views';
-
-            if ($filesystem->isDirectory($viewsPath)) {
-                $viewPaths[] = $viewsPath;
-            }
-        }
-
-        if ($viewPaths !== []) {
-            $this->loadViewsFrom($viewPaths, $module);
-        }
+        $this->loadViewsFrom($modulePath, $module);
     }
 }
 
