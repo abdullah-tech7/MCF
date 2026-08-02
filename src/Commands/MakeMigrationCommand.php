eb1a513c67cbf9bb073cdcd7e23346646d73995c
Abdullah Tech <abdullah.omar.tech.7@gmail.com>
2026-08-02 18:42:15 +0300

    Test changes

--- src/Commands/MakeMigrationCommand.php
+++ src/Commands/MakeMigrationCommand.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace MCF\Commands;
 
 use Illuminate\Database\Migrations\MigrationCreator;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Composer;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 use MCF\Database\Migrations\MigrateMakeCommand;
 
 class MakeMigrationCommand extends MigrateMakeCommand
@@ -32,7 +35,32 @@ class MakeMigrationCommand extends MigrateMakeCommand
         parent::__construct($creator, $composer);
     }
 
-    protected function getMigrationPath()
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        if ($this->migrationExists()) {
+            $this->components->error(
+                "Migration [{$this->getMigrationName()}] already exists."
+            );
+
+            return self::FAILURE;
+        }
+
+        $result = parent::handle();
+
+        if ($result === self::SUCCESS) {
+            $this->organizeMigrations();
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get the migration path.
+     */
+    protected function getMigrationPath(): string
     {
         $path = app_path('MCF/Database/Migrations');
 
@@ -41,5 +69,99 @@ class MakeMigrationCommand extends MigrateMakeCommand
         }
 
         return $path;
+    }
+
+    /**
+     * Organize migration files.
+     */
+    protected function organizeMigrations(): void
+    {
+        $this->renameMigrationFiles(
+            $this->getMigrationFiles()
+        );
+    }
+
+    /**
+     * Get all migration files.
+     */
+    protected function getMigrationFiles(): Collection
+    {
+        return collect(File::files($this->getMigrationPath()))
+            ->sortBy(fn ($file) => $file->getFilename())
+            ->values();
+    }
+
+    /**
+     * Rename migration files using sequential numbering.
+     */
+    protected function renameMigrationFiles(Collection $files): void
+    {
+        $path = $this->getMigrationPath();
+
+        foreach ($files as $index => $file) {
+
+            $name = $this->stripMigrationPrefix(
+                $file->getFilename()
+            );
+
+            $newFilename = sprintf(
+                '%04d_%s',
+                $index + 1,
+                $name
+            );
+
+            if ($file->getFilename() === $newFilename) {
+                continue;
+            }
+
+            File::move(
+                $file->getPathname(),
+                $path . DIRECTORY_SEPARATOR . $newFilename
+            );
+        }
+    }
+
+    /**
+     * Determine if the migration already exists.
+     */
+    protected function migrationExists(): bool
+    {
+        $migration = $this->getMigrationName();
+
+        foreach ($this->getMigrationFiles() as $file) {
+
+            $name = pathinfo(
+                $this->stripMigrationPrefix($file->getFilename()),
+                PATHINFO_FILENAME
+            );
+
+            if ($name === $migration) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Remove Laravel timestamp or MCF numbering.
+     */
+    protected function stripMigrationPrefix(string $filename): string
+    {
+        return preg_replace(
+            '/^(?:\d{4}_\d{2}_\d{2}_\d{6}|\d{4})_/',
+            '',
+            $filename
+        );
+    }
+
+    /**
+     * Get normalized migration name.
+     */
+    protected function getMigrationName(): string
+    {
+        return Str::snake(
+            $this->argument('name')
+        );
     }
 }
