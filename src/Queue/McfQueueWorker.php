e1e0a2c33cd5c498f3035485e5b55601416a00c1
Abdullah Tech <abdullah.omar.tech.7@gmail.com>
2026-08-19 02:08:01 +0300

    Fix background queue worker

--- src/Queue/McfQueueWorker.php
+++ src/Queue/McfQueueWorker.php
@@ -10,22 +10,39 @@ final class McfQueueWorker
     {
     }
 
-   public static function start(): bool
-{
-    $php = escapeshellarg(
-        PHP_BINARY,
-    );
+    public static function start(): bool
+    {
+        $php = PHP_BINARY;
+        $artisan = base_path('artisan');
 
-    $artisan = escapeshellarg(
-        base_path('artisan'),
-    );
+        if (PHP_OS_FAMILY === 'Windows') {
+            $command = sprintf(
+                'start "" /B cmd /D /S /C ""%s" "%s" queue:work --stop-when-empty"',
+                $php,
+                $artisan,
+            );
 
-    $command = sprintf(
-        '%s %s queue:work --stop-when-empty',
-        $php,
-        $artisan,
-    );
+            exec(
+                $command,
+                $output,
+                $status,
+            );
 
-    dd($command);
-}
+            return $status === 0;
+        }
+
+        $command = sprintf(
+            'nohup %s %s queue:work --stop-when-empty > /dev/null 2>&1 &',
+            escapeshellarg($php),
+            escapeshellarg($artisan),
+        );
+
+        exec(
+            $command,
+            $output,
+            $status,
+        );
+
+        return $status === 0;
+    }
 }
