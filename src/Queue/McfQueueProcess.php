baf0c6b14c164b832b4597828ae11032fbac08df
Abdullah Tech <abdullah.omar.tech.7@gmail.com>
2026-08-19 02:28:32 +0300

    Update changes

--- src/Queue/McfQueueProcess.php
+++ src/Queue/McfQueueProcess.php
@@ -16,21 +16,63 @@ final class McfQueueProcess
         $artisan = base_path('artisan');
 
         if (PHP_OS_FAMILY === 'Windows') {
-            $command = sprintf(
-                'start "" /B cmd /D /S /C ""%s" "%s" queue:work --once > NUL 2>&1"',
+            return self::startWindows(
                 $php,
                 $artisan,
             );
-
-            exec(
-                $command,
-                $output,
-                $status,
-            );
-
-            return $status === 0;
         }
 
+        return self::startUnix(
+            $php,
+            $artisan,
+        );
+    }
+
+    private static function startWindows(
+        string $php,
+        string $artisan,
+    ): bool {
+        $command = sprintf(
+            'start "" /B "%s" "%s" queue:work --once',
+            $php,
+            $artisan,
+        );
+
+        $process = proc_open(
+            $command,
+            [
+                0 => [
+                    'file',
+                    'NUL',
+                    'r',
+                ],
+                1 => [
+                    'file',
+                    'NUL',
+                    'w',
+                ],
+                2 => [
+                    'file',
+                    'NUL',
+                    'w',
+                ],
+            ],
+            $pipes,
+        );
+
+        if ($process === false) {
+            return false;
+        }
+
+        proc_close($process);
+
+        return true;
+    }
+
+    private static function startUnix(
+        string $php,
+        string $artisan,
+    ): bool {
         $command = sprintf(
             'nohup %s %s queue:work --once > /dev/null 2>&1 &',
             escapeshellarg($php),
